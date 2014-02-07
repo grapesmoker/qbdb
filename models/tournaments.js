2bedff5c3829467d36efff5ee5efb1dcc73b1cff
Arnav Sastry <arnav@heimdall>
2014-02-07 17:27:24 -0500

    nvm

--- models/tournaments.js
+++ models/tournaments.js
@@ -2,15 +2,15 @@ var mongoose = require('mongoose');
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var TournamentSchema = new mongoose.Schema({
-	tournament: String,
-	year: String,
-	numPackets: Number,
-	powers: Boolean,
-	packets: [{type: ObjectId, ref: 'Packet'}]
+  tournament: String,
+  year: String,
+  numPackets: Number,
+  powers: Boolean,
+  packets: [{type: ObjectId, ref: 'Packet'}]
 });
 
 var Tournament = mongoose.model('Tournament', TournamentSchema);
 
 module.exports = {
-	Tournament: Tournament
+  Tournament: Tournament
 };
